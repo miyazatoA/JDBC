b8f63e977adc241996425115aae1b0bcd27807fd
BBC300190 <BBC300190@E65250.cmjgwd.canon-mj.co.jp>
2017-05-30 00:00:30 +0900

    接続プール確認用モジュールのコミット

--- JDBC/src/java/answer/jp/co/edu/ds/DBUtil.java
+++ JDBC/src/java/answer/jp/co/edu/ds/DBUtil.java
@@ -11,68 +11,44 @@ import javax.sql.DataSource;
 
 // TODO BasicDataSourceがDerbyで適用できるか確認
 //import org.apache.commons.dbcp.BasicDataSource;
-
 public final class DBUtil {
 
-	private static DataSource dataSource = null;
+    private static DataSource dataSource = null;
 
-	private DBUtil() {
-	}
+    private DBUtil() {
+    }
 
-	public synchronized static Connection getConnection() throws SQLException {
+    public synchronized static Connection getConnection() throws SQLException {
 
-		if (DBUtil.dataSource == null) {
+        try {
+            InitialContext ctx = new InitialContext();
+            dataSource = (DataSource) ctx.lookup("jdbc/sample");
 
-			// DB接続情報
-//                        String className = "org.apache.derby.jdbc.ClientDriver";
-//                        String url = "jdbc:derby://localhost:1527/SQLTEST";
-//                        String user = "sqltest";
-//                        String password = "sqltest";
-			int initialSize = 10;
-			int maxActive = 20;
+            Connection connection = DBUtil.dataSource.getConnection();
+            System.out.println("DataSourceで接続");
+            return connection;
+        } catch (SQLException | NamingException e) {
+            throw new SQLException("DB接続エラーが発生しました。", e);
+        }
+    }
 
-//			BasicDataSource basicDataSource = new BasicDataSource();
-//			basicDataSource.setDriverClassName(className);
-//			basicDataSource.setUrl(url);
-//			basicDataSource.setUsername(user);
-//			basicDataSource.setPassword(password);
-//			basicDataSource.setInitialSize(initialSize);
-//			basicDataSource.setMaxActive(maxActive);
-//
-//			DBUtil.dataSource = basicDataSource;
+    public static void close(Connection connection,
+            PreparedStatement preparedStatement, ResultSet resultSet)
+            throws SQLException {
 
-
-                }
-
-		try {
-                        InitialContext ctx = new InitialContext();
-                        DataSource ds = (DataSource)ctx.lookup("java:app/jdbc/SamplePool");
-
-                        Connection connection = DBUtil.dataSource.getConnection();
-                        System.out.println("DataSourceで接続");
-			return connection;
-		} catch (SQLException | NamingException e) {
-			throw new SQLException("DB接続エラーが発生しました。", e);
-		}
-	}
-
-	public static void close
-			(Connection connection, PreparedStatement preparedStatement, ResultSet resultSet)
-			throws SQLException {
-
-		try {
-			if (resultSet != null) {
-				resultSet.close();
-			}
-			if (preparedStatement != null) {
-				preparedStatement.close();
-			}
-			if (connection != null) {
-				connection.close();
-			}
-		} catch (SQLException e) {
-			throw new SQLException("DB接続解放エラーが発生しました。", e);
-		}
-	}
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            throw new SQLException("DB接続解放エラーが発生しました。", e);
+        }
+    }
 
 }
--- JDBC/src/java/answer/jp/co/edu/ds/LoadTest_main.java
+++ JDBC/src/java/answer/jp/co/edu/ds/LoadTest_main.java
@@ -7,39 +7,64 @@ import java.sql.SQLException;
 import answer.jp.co.edu.dto.ProductDto;
 import answer.jp.co.edu.logic.product.dao.ProductQueryDao;
 import answer.jp.co.edu.logic.product.dao.ProductUpdateDao;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+public class LoadTest_main extends HttpServlet {
 
-public class LoadTest_main {
+    private static ProductUpdateDao productUpdateDao = new ProductUpdateDao();
+    private static ProductQueryDao productQueryDao = new ProductQueryDao();
+    private static Connection connection = null;
 
-	private static ProductUpdateDao productUpdateDao = new ProductUpdateDao();
-	private static ProductQueryDao productQueryDao = new ProductQueryDao();
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            testSetup();
+            testLoad();
 
-	public static void main(String[] args) throws Exception{
-		testSetup();
-		
-		testLoad();
-	}
-	
-	
-	
-	public static void testSetup() throws SQLException {
-		Connection connection = DBUtil.getConnection();
-		connection.close();
-	}
+            response.setContentType("text/plain; charset=Windows-31J");
+            PrintWriter out = response.getWriter();
+            out.println("testLoad実行完了");
+        } catch (SQLException e) {
+            System.out.println(e);
 
-	public static void testLoad() throws Exception {
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            try {
+                testShutdown();
+            } catch (SQLException e) {
+                System.out.println(e);
+            }
+        }
+    }
 
-		Long testCategoryId = 1L;
-		String testName = "テスト負荷名称";
-		BigDecimal testWeight = new BigDecimal("1999");
+    public static void testSetup() throws SQLException {
+        connection = DBUtil.getConnection();
+    }
 
-		ProductDto productDto = new ProductDto(null, testCategoryId, testName, testWeight);
+    public static void testLoad() throws Exception {
 
-		for (int i = 0; i < 2; i++) {
-			productUpdateDao.insertGenerateKey(productDto);
-			productUpdateDao.update(productDto);
-			productQueryDao.selectLikeName("負荷");
-			productUpdateDao.delete(productDto);
-		}
-	}
+        Long testCategoryId = 1L;
+        String testName = "テスト負荷名称";
+        BigDecimal testWeight = new BigDecimal("1999");
+
+        ProductDto productDto = new ProductDto(null, testCategoryId, testName, testWeight);
+
+        for (int i = 0; i < 2; i++) {
+            productUpdateDao.insertGenerateKey(productDto);
+            productUpdateDao.update(productDto);
+            productQueryDao.selectLikeName("負荷");
+//            productUpdateDao.delete(productDto);
+        }
+    }
+
+    public static void testShutdown() throws SQLException {
+        connection.close();
+    }
+
 }
